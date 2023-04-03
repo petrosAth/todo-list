21a1daa0b6654f5acf36c7716d3100d702dc9d42
petrosAth <develop@athanasoulis.me>
2023-04-03 19:04:05 +0300

    Clean comments

--- src/scripts/modules/useList.js
+++ src/scripts/modules/useList.js
@@ -3,10 +3,6 @@ import { storeItem, loadAllItems } from './utilities.js';
 const createList = (list, title) => {
   const id = crypto.randomUUID();
   const newList = new list(title, id);
-  // const listProps = {
-  //   [id]: list.name,
-  //   title: title,
-  // };
 
   storeItem(id, {
     [id]: list.name,
--- src/scripts/modules/useTask.js
+++ src/scripts/modules/useTask.js
@@ -3,13 +3,6 @@ import { storeItem, loadAllItems } from './utilities.js';
 const createTask = (task, title, priority, dueDate, description) => {
   const id = crypto.randomUUID();
   const newTask = new task(id, title, priority, dueDate, description);
-  // const taskProps = {
-  //   [id]: task.name,
-  //   title: title,
-  //   priority: priority,
-  //   dueDate: dueDate,
-  //   description: description,
-  // };
 
   storeItem(id, {
     [id]: task.name,
