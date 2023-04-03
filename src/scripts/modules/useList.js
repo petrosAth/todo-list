1b0d2e8fe3ce70d599eaad5f07e861175f9d79d9
petrosAth <develop@athanasoulis.me>
2023-04-03 22:23:29 +0300

    Add helper functions in useList module

--- src/scripts/modules/useList.js
+++ src/scripts/modules/useList.js
@@ -1,15 +1,48 @@
 import { storeItem, loadAllItems } from './utilities.js';
 
-const createList = (list, title) => {
-  const id = crypto.randomUUID();
-  const newList = new list(title, id);
+const masterList = () => {
+  const masterList = [];
 
-  storeItem(id, {
-    [id]: list.name,
-    title: title,
-  });
+  const _getListIndex = (id) => {
+    return masterList.findIndex((list) => list.info.id === id);
+  };
 
-  return newList;
+  const _add = (listObject) => {
+    masterList.push(listObject);
+  };
+
+  const create = (listClass, title, id) => {
+    const newList = new listClass(title, id);
+    _add(newList);
+    storeItem(newList.info.id, {
+      [newList.info.id]: listClass.name,
+      title: newList.info.name,
+    });
+  };
+
+  const rename = (id, title) => {
+    masterList[_getListIndex(id)].name = title;
+  };
+
+  const addTask = (listId, task) => {
+    masterList[_getListIndex(listId)].addTask(task);
+  };
+
+  const removeTask = (listId, taskId) => {
+    masterList[_getListIndex(listId)].removeTask(taskId);
+  };
+
+  return {
+    showAll: masterList,
+    create,
+    rename,
+    addTask,
+    removeTask,
+  };
 };
 
-export { createList };
+const loadAllLists = (listClasses) => {
+  loadAllItems(listClasses, ['title']);
+};
+
+export { loadAllLists, masterList };
