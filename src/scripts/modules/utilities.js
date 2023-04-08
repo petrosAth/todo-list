f10c37747dc704aab1c7ae1838698f7dbe6e8f3c
petrosAth <develop@athanasoulis.me>
2023-04-08 12:14:04 +0300

    Add node creation utility functions

--- src/scripts/modules/utilities.js
+++ src/scripts/modules/utilities.js
@@ -21,4 +21,53 @@ const dateFormat = () => {
   };
 };
 
-export { dateFormat };
+const createNewNode = (element) => document.createElement(element);
+
+const addClassesAndId = (node, classesAndId) => {
+  if (classesAndId.classes) {
+    node.classList.add(...[].concat(classesAndId.classes));
+  }
+  if (classesAndId.id) {
+    node.setAttribute('id', classesAndId.id);
+  }
+};
+
+const addContent = (node, content) => {
+  const element = {
+    IMG: () => (node.src = content.image || ''),
+    A: () => {
+      node.href = content.link || '';
+      node.textContent = content.text || '';
+      node.target = content.newTab || '';
+    },
+  };
+
+  if (node.nodeName in element) {
+    element[node.nodeName]();
+  } else {
+    node.textContent = content.text;
+  }
+};
+
+const createNewElement = (element, classesAndId, content) => {
+  const newNode = createNewNode(element);
+
+  if (classesAndId) addClassesAndId(newNode, classesAndId);
+  if (content) addContent(newNode, content);
+
+  return newNode;
+};
+
+const appendChildren = (elementPairs) => {
+  elementPairs.forEach((pair) => {
+    if (Array.isArray(pair[1])) {
+      pair[1].forEach((child) => {
+        pair[0].appendChild(child);
+      });
+    } else {
+      pair[0].appendChild(pair[1]);
+    }
+  });
+};
+
+export { createNewElement, appendChildren, dateFormat };
